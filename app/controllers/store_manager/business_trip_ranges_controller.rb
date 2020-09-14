18295771833415e2bf8bfdbe270d353235114a87
松本隼 <matsumotoshun@matsumotoshunnoMacBook-Pro.local>
2020-09-14 23:21:41 +0900

    実験前　カラムを追加する前

--- app/controllers/store_manager/business_trip_ranges_controller.rb
+++ app/controllers/store_manager/business_trip_ranges_controller.rb
@@ -1,23 +1,24 @@
 class StoreManager::BusinessTripRangesController < StoreManager::Base
-  
+
   include StoreManager::BusinessTripRangesHelper
-  
+
   before_action :set_masseur, only: [:edit, :update, :show]
   before_action :corrrect_store_manager, only: [:edit, :update, :show]
-  
+
   def index
     @store = current_store_manager.store
     @masseurs = @store.masseurs
   end
-  
-  
-  
+
+
+
   def edit
     @prefectures = Prefecture.all
     @ranges = @current_masseur.business_trip_ranges
   end
-  
+
   def update
+    debugger
     # チェックがあった場合
     if params[:masseur].present?
       @current_masseur.update(city_business_trip_range_params)
@@ -29,31 +30,31 @@ class StoreManager::BusinessTripRangesController < StoreManager::Base
       render :edit
     end
   end
-  
+
   def show
     @business_trip_ranges = @current_masseur.business_trip_ranges
     # 閲覧中のマッサージ師が持っている出張範囲(市町村)が属している都道府県IDを配列にして取得
-    @prefecture_ids = @current_masseur.business_trip_ranges.pluck(:city_id).map {|id| City.find_by(id: id).prefecture_id } 
+    @prefecture_ids = @current_masseur.business_trip_ranges.pluck(:city_id).map {|id| City.find_by(id: id).prefecture_id }
     # 閲覧中のマッサージ師が持っているcity_idを配列で取得
     @city_ids = @current_masseur.business_trip_ranges.pluck(:city_id)
   end
-  
+
   def city_select
     init(params[:prefecture_id].to_i)
   end
-  
+
   private
-  
+
   def set_masseur
     @current_masseur = Masseur.find(params[:masseur_id])
   end
-  
+
   def city_business_trip_range_params
     if params[:masseur].present?
-      params.require(:masseur).permit(city_ids: [])
+      params.require(:masseur).permit(city_ids12: [], city_ids13: [], city_ids14: [])
     end
   end
-  
+
   def corrrect_store_manager
     unless current_store_manager.store.masseurs.ids.include?params[:masseur_id].to_i
       flash[:danger] = "アクセス権限がありません。"
