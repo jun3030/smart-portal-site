9fcc5de5ed5bb3b027d4ee1a9b0549f79bbb2d8d
MIDORIMOTONISHI <midoo.nijo@gmail.com>
2020-11-06 08:19:32 +0900

    1106ここからリプライモデル消していく

--- app/controllers/store_manager/top_page_controller.rb
+++ app/controllers/store_manager/top_page_controller.rb
@@ -8,7 +8,6 @@ class StoreManager::TopPageController < StoreManager::Base
                             「登録内容の編集」から有料プランをご契約いただきますとページが公開され、お客様からのご予約が可能となります。".html_safe
     end
     @messages = Message.where(checked: "未読", store_id: @store.id).count
-    @overtime_notice = Attendance.where(overtime_status: "申請中", confirmation: @user.name).count
   end
 
   def update_calendar_status
