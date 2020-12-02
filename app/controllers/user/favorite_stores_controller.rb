cd1539ced632d1ace0f3f633006a819b6ff25ece
松本隼 <matsumotoshun@matsumotoshunnoMacBook-Pro.local>
2020-12-02 20:32:46 +0900

    　お気に入り機能完成

--- app/controllers/user/favorite_stores_controller.rb
+++ app/controllers/user/favorite_stores_controller.rb
@@ -9,23 +9,20 @@ class User::FavoriteStoresController < User::Base
   def create
     store=Store.find(params[:store_id])
     if FavoriteStore.create(user_id: current_user.id,store_id:store.id)
-      flash[:success] = "お気に入り店舗に登録しました。"
+      flash.now[:success] = "お気に入り店舗に登録しました。"
       redirect_to details_url(store)
     else
-      flash[:success] = "お気に入り店舗の登録に失敗しました。お手数ですがもう一度やり直して下さい。"
+      flash.now[:success] = "お気に入り店舗の登録に失敗しました。お手数ですがもう一度やり直して下さい。"
       redirect_to details_url(store)
     end
   end
 
   def destroy
-    debugger
     store=Store.find(params[:store_id])
-    if favorite=FavoriteStore.find_by(user_id: current_user.id,store_id:store.id)
-      favorite.delete
-      redirect_to root_url
-    else
-      redirect_to root_url
-    end
+    favorite=FavoriteStore.find_by(user_id: current_user.id,store_id:store.id)
+    favorite.destroy
+    flash[:success] = "#{store.store_name}をお気に入りの店舗から削除しました。"
+    redirect_to user_favorite_stores_index_url(current_user)
   end
 
   private
