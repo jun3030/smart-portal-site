7c42d0bda4a1c2d4ec829d5b87e69e2cc5b2365c
MIDORIMOTONISHI <midoo.nijo@gmail.com>
2020-12-28 11:57:47 +0900

    1228プルリク

--- spec/system/message_spec.rb
+++ spec/system/message_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe 'Message', type: :system do
     context "sign_in状態の場合" do
       before do
         sign_in
-        visit details_path(@store.id)
+        visit details_path(@store.id) # 店舗詳細ページへ
         expect(page).to have_content '店舗への問い合わせはこちら' # メッセージボタンが存在することを確認
-        visit store_message_new_path(@store.id)
+        visit store_message_new_path(@store.id) # メッセージ作成ページへ
       end
       context "値がvalidの場合" do
         it "メッセージが送信される" do
@@ -22,76 +22,47 @@ RSpec.describe 'Message', type: :system do
           fill_in 'タイトル', with: 'プランについて'
           fill_in '内容', with: 'もう少し長い施術時間のコースはありますか？'
           click_on 'メッセージを送信する'
-          find('#store_id', visible: false).set('@store.id')　# hidden
-          find('#user_id', visible: false).set('current_user.id') # hidden
-          find('#checked', visible: false).set('未読') # hidden
+          find('#store_id', visible: false).set('@store.id')　# hidden_field
+          find('#user_id', visible: false).set('current_user.id') # hidden_field
+          find('#checked', visible: false).set('未読') # hidden_field
           expect(current_path).to eq details_path(@store.id) # 店舗詳細ページへ遷移したか確認
           expect(page).to have_content 'メッセージを送信しました。' # メッセージが表示されたか確認
-          expect(Message.all.count).not_to eq(before_count) # 口コミが作成されたか確認
+          expect(Message.all.count).not_to eq(before_count) # メッセージが作成されたか確認
         end
       end
       context "値がinvalidの場合" do
-        it "口コミ投稿に失敗する" do
-          find('#star').find("img[alt='5']").click
-          fill_in 'タイトル', with: 'また利用したいです'
-          fill_in 'レビュー内容', with: ''
-          click_on '口コミを投稿する'
-          expect(page).to have_content '口コミが投稿できませんでした。投稿するには全ての項目を埋めて下さい。' # エラーメッセージが出ているか確認/newページに遷移したか確認
-          expect(page).to have_field 'タイトル', with: 'また利用したいです' # 入力内容が保持されているか確認
-          expect { click_on '口コミを投稿する' }.not_to change(Review, :count) # 口コミの作成に失敗したか確認
+        it "メッセージ送信に失敗する" do
+          fill_in 'タイトル', with: 'プランについて'
+          fill_in '内容', with: ''
+          click_on 'メッセージを送信する'
+          expect(page).to have_content '本文を入力してください' # エラーメッセージが出ているか確認
+          expect(current_path).to eq store_message_new_path(@store.id) # message_newページに遷移したか確認
+          expect(page).to have_field 'タイトル', with: 'プランについて' # 入力内容が保持されているか確認
+          expect { click_on 'メッセージを送信する' }.not_to change(Message, :count) # メッセージの作成に失敗したか確認
         end
       end
-      context "口コミが投稿された場合" do
+      context "メッセージが作成された場合" do
         before do
-          find('#star').find("img[alt='5']").click
-          fill_in 'タイトル', with: 'また利用したいです'
-          fill_in 'レビュー内容', with: '丁寧に施術して頂きました。'
-          click_on '口コミを投稿する'
-          expect(current_path).to eq user_store_review_index_path(@store.id) # indexへ遷移したか確認
+          fill_in 'タイトル', with: 'プランについて'
+          fill_in '内容', with: 'もう少し長い施術時間のコースはありますか？'
+          click_on 'メッセージを送信する'
+          find('#store_id', visible: false).set('@store.id')　# hidden_field
+          find('#user_id', visible: false).set('current_user.id') # hidden_field
+          find('#checked', visible: false).set('未読') # hidden_field
         end
-        it "showページへ遷移できること" do
-          click_on '丁寧に施術して頂きました。'
-          expect(page).to have_content '編集' # 編集ボタンがあることを確認
-          expect(page).to have_content '削除' # 編集ボタンがあることを確認
+        it "message_showページへ遷移できること" do
+          click_on 'プランについて'
+          expect(page).to have_content '削除' # 削除ボタンがあることを確認
         end
-        context "editページへ遷移した場合" do
-          context "値がvalidの場合" do
-            it "口コミを更新できること" do
-              click_on '丁寧に施術して頂きました。'
-              click_on '編集'
-              find('#star').find("img[alt='1']").click
-              fill_in 'タイトル', with: '更新'
-              fill_in 'レビュー内容', with: '体が楽になりました！'
-              click_on '口コミを編集する'
-              expect(current_path).to eq user_store_review_show_path(@store.id, Review.first.id)
-              expect(page).to have_content '口コミを更新しました。'
-            end
-          end
-          context "値がinvalidの場合" do
-            it "口コミの更新に失敗すること" do
-              click_on '丁寧に施術して頂きました。'
-              click_on '編集'
-              find('#star').find("img[alt='1']").click
-              fill_in 'タイトル', with: '更新失敗'
-              fill_in 'レビュー内容', with: ''
-              click_on '口コミを編集する'
-              expect(page).to have_content '口コミが更新されませんでした。更新するには全ての項目を記入して下さい。' # エラーメッセージが出ているか確認/editページに遷移したか確認
-              expect(page).to have_field 'タイトル', with: '更新失敗' # 入力内容が保持されているか確認
-              expect(page).to have_field 'レビュー内容', with: '' # 入力内容が保持されているか確認
-            end
-          end
-          context "削除を押した場合" do
-            let!(:before_count) { Review.all.count }
-            it "口コミを削除できること" do
-              click_on '丁寧に施術して頂きました。'
-              click_on '削除'
-              expect(page.driver.browser.switch_to.alert.text).to eq "口コミを本当に削除しますか？" # アラートメッセージの確認
-              page.driver.browser.switch_to.alert.dismiss
-              expect{
-                  expect(page.accept_confirm).to eq "口コミを本当に削除しますか？"
-                  expect(page).to have_content "口コミを削除しました。"
-                  }
-            end
+        context "削除を押した場合" do
+          let!(:before_count) { Message.all.count }
+          it "メッセージを削除できること" do
+            click_on '削除'
+            expect(page.driver.browser.switch_to.alert.text).to eq "削除してよろしいですか？" # アラートメッセージの確認
+            page.driver.browser.switch_to.alert.dismiss
+            expect{
+                expect(page).to have_content "口コミを削除しました。"
+                }
           end
         end
       end
